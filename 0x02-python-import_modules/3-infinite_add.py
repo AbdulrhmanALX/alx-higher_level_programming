79a673e857072498b8c4f10df4a10a167a16eb22
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-12 20:50:48 +0300

    2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__" :
+if __name__ == "__main__":
     x = 0
-    for _ in argv[1:] :
+    for _ in argv[1:]:
         x += int(_) 
     print(x) 
