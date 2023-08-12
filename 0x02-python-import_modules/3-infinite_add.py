935171a6e88abac692875bb2a0e43d04d6c2eb2f
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-12 20:54:21 +0300

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    x = 0
-    for _ in argv[1:]:
-        x += int(_) 
-    print(x) 
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argc = len(sys.argv) - 1
+
+i = 0
+result = 0
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
+    else:
+        i += 1
+print("{:d}".format(result))
