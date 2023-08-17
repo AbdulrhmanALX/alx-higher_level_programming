bd6b402837ad5c426305fbc65e58dba0a7e1e293
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-17 22:45:06 +0300

    1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Prints all intergers of a list
+
+    Args:
+      my_list: the list argument whose items will be printed
+"""
+def print_list_integer(my_list=[]):
+    for element in list:
+        pprint("{:d}".format(integer))
