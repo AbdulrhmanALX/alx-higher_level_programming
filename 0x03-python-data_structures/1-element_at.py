62c558100238343aaf2ce44c8e8d1f4cb29407c7
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-18 10:46:46 +0300

    1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """return element of a list
+
+    Args:
+        my_list (list): _description_
+        idx (int): _description_
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return('None')
+    else:
+        return(my_list[idx])
