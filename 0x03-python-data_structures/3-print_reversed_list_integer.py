d2d52d854967c5184e02fc1c6afcf7e930edc59c
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-18 12:36:18 +0300

    1

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Retrieves an element
+
+    Args:
+        my_list: a list
+        idx: the index of item to retrieve
+
+    """
+
+    my_list.reverse()
+    for _ in my_list:
+        print("{:d}".format(_))
+    
