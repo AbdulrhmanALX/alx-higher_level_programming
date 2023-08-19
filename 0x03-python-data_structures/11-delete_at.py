531b4fc6197cbc1b6ba590672a46b253c589ce8d
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-19 19:09:28 +0300

    1

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """Retrieves an element
+
+    Args:
+        my_list: a list
+        idx: the index of item to retrieve
+
+    """
+
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list.remove(idx)
+    return my_list
