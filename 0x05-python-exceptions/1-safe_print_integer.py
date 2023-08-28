236aecb38d2935a9256d3e3e3819e76fbfb1b083
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-28 18:51:45 +0300

    docstrip

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    """_summary_
+
+    Args:
+        value (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
     try:
         print("{:d}".format(value))
         return True
