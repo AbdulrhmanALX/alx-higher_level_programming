cc0f76a10a2d3e089b2ee6893812f476526143de
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-29 18:12:31 +0300

    4

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-    """_summary_
-    """
-class Square:
-    """_summary_
-    """
-    def __init__(self, size):
-        """_summary_
+"""Square Class.
 
-        Args:
-            size (_type_): _description_
+This module contains a class that defines a square.
+
+Usage Example:
+
+    Square = __import__('1-square').Square
+
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
+
+
+class Square:
+    """Defines the blueprint of a square.
+
+    Attribute:
+        size: An integer indicating the size of the square object.
+    """
+
+    def __init__(self, size):
+        """An object constructor method.
+
+        Initiatilizes Square with size.
+
+        Arg:
+            size: A integer representing object size
         """
         self.__size = size
