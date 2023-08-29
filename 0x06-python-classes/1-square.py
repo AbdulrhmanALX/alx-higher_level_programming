273a39b317cae8fb631d0b9001103b93359b1ee9
Abdelrahman <bodyronaldo30@gmail.com>
2023-08-29 18:09:18 +0300

    3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,11 @@
     """
 class Square:
     """_summary_
-    """   
+    """
     def __init__(self, size):
         """_summary_
 
         Args:
             size (_type_): _description_
-        """        
+        """
         self.__size = size
