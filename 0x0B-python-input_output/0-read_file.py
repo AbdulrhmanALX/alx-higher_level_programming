fc36a76e59b2ea35fbabb8da0d974eda326199e7
Abdelrahman <bodyronaldo30@gmail.com>
2023-09-16 19:12:26 +0300

    1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """_summary_
+    """
+def read_file(filename=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """    
+    with open(filename,'r', encode="UTF8") as file:
+        print(file.read())
