7ed13d314a75fa7cd388381b932da86891726496
Abdelrahman <bodyronaldo30@gmail.com>
2023-09-16 19:57:16 +0300

    1

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 1-write_file.
+Writes in a text file.
+"""
+import json
+
+def to_json_string(my_obj):
+    """Writes text in filename.
+    Args:
+        - filename: name of the file
+        - text: string to write in the file
+    Returns: number of characters written
+    """
+    
+    return json.loads(my_obj)
