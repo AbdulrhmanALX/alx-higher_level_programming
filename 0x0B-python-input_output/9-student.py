2f040ee1d3423f12a99217a7edf421752bbbbcaf
Abdelrahman <bodyronaldo30@gmail.com>
2023-09-18 18:41:06 +0300

    2

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,32 @@
+#!/usr/bin/python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+
 import json
 
 class Student:
+    """_summary_
+    """    
     
     def __init__(self, first_name, last_name, age):
+        """_summary_
+
+        Args:
+            first_name (_type_): _description_
+            last_name (_type_): _description_
+            age (_type_): _description_
+        """        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         
     def to_json(self):
-        return self.__dict__
-    
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+        """_summary_
 
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+        Returns:
+            _type_: _description_
+        """        
+        return self.__dict__
