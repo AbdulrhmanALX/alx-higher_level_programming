c0506c4295e18fcad31a595042cfd257b1697978
Abdelrahman <bodyronaldo30@gmail.com>
2023-11-27 20:56:09 +0200

    1^^

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,15 +12,35 @@ class Rectangle:
     """Defines the blueprint of a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """_summary_
+
+        Args:
+            width (int, optional): _description_. Defaults to 0.
+            height (int, optional): _description_. Defaults to 0.
+        """        
         self.__width = width
         self.__hight = height
     
     @property    
     def width(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """        
         return self.__width
     
     @width.setter
     def width(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+
+        Raises:
+            ValueError: _description_
+            TypeError: _description_
+        """        
         if type(value) == int:
             if value >= 0:
                 self.__width = value
@@ -31,10 +51,24 @@ class Rectangle:
      
     @property    
     def height(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """        
         return self.__hight
     
     @height.setter
     def height(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+
+        Raises:
+            ValueError: _description_
+            TypeError: _description_
+        """        
         if type(value) == int:
             if value >= 0:
                 self.__hight = value
