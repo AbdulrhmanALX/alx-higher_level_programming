716b6b1e6fc0cbb653da22d3e11eea670c788ba5
Abdelrahman <bodyronaldo30@gmail.com>
2023-12-27 21:45:09 +0200

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+    """
+    return isinstance(obj, a_class)   
