c9d617ff38a305eb8245a8484d80e7dd7a9a3f90
Abdelrahman <bodyronaldo30@gmail.com>
2024-01-03 21:26:44 +0200

    4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+imprt json
+def from_json_string(my_str):
+    """_summary_
+
+    Args:
+        my_str (_type_): _description_
+    """
+    py = json.loads(my_str)
+    retrun
